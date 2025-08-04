1882eb6a24f1dcb2c5dc1575c261f78c34863587
RezaBG <101470327+RezaBG@users.noreply.github.com>
2025-08-04 21:32:35 +0200

    feat: load environment variables and set default port

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default port
+	}
+
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
@@ -15,5 +28,5 @@ func main() {
 		})
 	})
 
-	r.Run() // listen and serve on ":8080"
+	r.Run(":" + port) // listen and serve on ":8080"
 }
