ceea2624be0ad2321e006f7724acb79521db2896
RezaBG <101470327+RezaBG@users.noreply.github.com>
2025-08-05 14:53:39 +0200

    feat: add DTOs for creating and updating products

---
+++ internal/product/dto.go
@@ -0,0 +1,15 @@
+package product
+
+type CreateProductInput struct {
+	Name        string  `json:"name" binding:"required"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price" binding:"required,gt=0"`
+	Quantity    int     `json:"quantity" binding:"required,gte=0"`
+}
+
+type UpdateProductInput struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price" binding:"gt=0"`
+	Quantity    int     `json:"quantity" binding:"gte=0"`
+}
