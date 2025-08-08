78fb223f0ad2ff751a7e39413788c0dfd9523d3e
RezaBG <101470327+RezaBG@users.noreply.github.com>
2025-08-08 09:52:50 +0200

    feat: add InventoryTransaction model with transaction types and fields

---
+++ internal/inventory/model.go
@@ -0,0 +1,27 @@
+package inventory
+
+import (
+	"github.com/RezaBG/Inventory-management-api/internal/user"
+
+	"github.com/RezaBG/Inventory-management-api/internal/product"
+	"gorm.io/gorm"
+)
+
+type TransactionType string
+
+const (
+	StockIn    TransactionType = "stock_in"
+	StockOut   TransactionType = "stock_out"
+	Adjustment TransactionType = "adjustment"
+)
+
+type InventoryTransaction struct {
+	gorm.Model
+	ProductID      uint            `json:"productID" gorm:"not null"`
+	Product        product.Product `json:"product"`
+	UserID         uint            `json:"userID" gorm:"not null"`
+	User           user.User       `json:"user"`
+	Type           TransactionType `json:"type" gorm:"not null"`
+	QuantityChange int             `json:"quantityChange" gorm:"not null"`
+	Notes          string          `json:"notes,omitempty"`
+}
